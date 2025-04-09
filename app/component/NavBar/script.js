c062ae05a16d50d5d77ed486f7664eafb744f4b4
[berkersuzan] <[sarah.berkersuzan@etu.unilim.fr]>
2025-04-09 14:41:17 +0200

    itération 6 fini

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -3,9 +3,20 @@ let template = await templateFile.text();
 
 let NavBar = {};
 
-NavBar.format = function (handlerMovies) {
+NavBar.format = function (handlerMovies, handlerProfil, profils) {
   let html = template;
   html = html.replace("{{handlerMovies}}", handlerMovies);
+  html = html.replace("{{handler}}", handlerProfil);
+
+  let profil = "";
+  for (let i = 0; i < profils.length; i++) {
+    let p = profils[i];
+    profil += `<option value="${p.nom}" data-img="${p.avatar}" data-dob="${p.age}">${p.nom}</option>`;
+  }
+
+  let avatar = profils[0]?.image || "";
+  html = html.replace("{{profil}}", profil);
+  html = html.replace("{{avatar}}", avatar);
   return html;
 };
 
